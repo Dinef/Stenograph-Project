e93baf1650a091f07bc34c33dcafa62d8dc943de
Dinef <shandinefacey@gmail.com>
2019-04-07 12:06:23 -0400

    Add files via upload
    Initial commit.
    This project uploads and display images. Encryption is not included.

---
+++ Default.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+
+namespace Testing
+{
+    public partial class _Default : Page
+    {
+
+
+
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                
+            }
+        }
+
+
+
+        protected void UploadFile(object sender, EventArgs e)
+        {
+            string folderPath = Server.MapPath("~/Files/");
+
+            //Check whether Directory (Folder) exists.
+            if (!Directory.Exists(folderPath))
+            {
+                //If Directory (Folder) does not exists Create it.
+                Directory.CreateDirectory(folderPath);
+            }
+
+            //Save the File to the Directory (Folder).
+            FileUpload1.SaveAs(folderPath + Path.GetFileName(FileUpload1.FileName));
+
+            //Display the Picture in Image control.
+            Image1.ImageUrl = "~/Files/" + Path.GetFileName(FileUpload1.FileName);
+        }
+
+
+    }
+}
