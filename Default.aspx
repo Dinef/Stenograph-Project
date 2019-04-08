2a84fc386e60d7b0b780dda0aa2910ea3d7fe007
Dinef <shandinefacey@gmail.com>
2019-04-08 15:24:35 -0400

    Encode and Decode filepath not properly defined

---
+++ Default.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Testing._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <h1>Stenography</h1>
+        
+    </div>
+
+    <div class="row">
+        <div class="col-md-4">
+                <asp:FileUpload ID="FileUpload1" runat="server" />
+                 <br />
+                <asp:Button ID="btnUpload" Text="Upload" CssClass="btn btn-primary"  runat="server" OnClick="UploadFile" />
+                <hr />
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+                <asp:Image ID="Image1" runat="server" Height = "300" Width = "300" />
+        </div>
+        <div class="col-md-4">
+            <asp:TextBox ID="txtMsg" runat="server" CssClass="form-control" 
+    Width="250px" TabIndex="1"></asp:TextBox>
+            <br />
+            
+            <asp:Button ID="btnEncode" Text="Encode" CssClass="btn btn-success"  runat="server" OnClick="btnEncode_Click" />
+
+            <br />
+
+                        <asp:Button ID="btnDecode" Text="Decode" CssClass="btn btn-danger"  runat="server" />
+
+            <asp:TextBox ID="txtDecodedMsg" runat="server"></asp:TextBox>
+
+         </div>
+     
+    </div>
+
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Drawing;
+using Testing.Model;
+
+
+namespace Testing
+{
+    public partial class _Default : Page
+    {
+        Stenography stenography = new Stenography();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                
+            }
+        }
+
+
+
+        protected void UploadFile(object sender, EventArgs e)
+        {
+            string folderPath = Server.MapPath("~/Files/");
+
+            //Check whether Directory (Folder) exists.
+            if (!Directory.Exists(folderPath))
+            {
+                //If Directory (Folder) does not exists Create it.
+                Directory.CreateDirectory(folderPath);
+            }
+
+            //Save the File to the Directory (Folder).
+            FileUpload1.SaveAs(folderPath + Path.GetFileName(FileUpload1.FileName));
+
+            //Display the Picture in Image control.
+            Image1.ImageUrl = "~/Files/" + Path.GetFileName(FileUpload1.FileName);
+
+
+        }
+
+      
+
+      
+
+        protected void btnEncode_Click(object sender, EventArgs e)
+        {
+             String filepath = "//Mac/Home/Pictures/Camera Roll/beach.jpg";
+            
+            String message = txtMsg.Text;
+            Bitmap img = new Bitmap(filepath);
+
+            for (int i = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    Color pixel = img.GetPixel(i, j);
+
+                    if (i < 1 && j < message.Length)
+                    {
+                        Console.WriteLine("R = [" + i + "][" + j + "] = " + pixel.R);
+                        Console.WriteLine("G = [" + i + "][" + j + "] = " + pixel.G);
+                        Console.WriteLine("G = [" + i + "][" + j + "] = " + pixel.B);
+
+                        char letter = Convert.ToChar(message.Substring(j, 1));
+                        int value = Convert.ToInt32(letter);
+                        Console.WriteLine("letter : " + letter + " value : " + value);
+
+                        img.SetPixel(i, j, Color.FromArgb(pixel.R, pixel.G, value));
+                    }
+
+                    if (i == img.Width - 1 && j == img.Height - 1)
+                    {
+                        img.SetPixel(i, j, Color.FromArgb(pixel.R, pixel.G, message.Length));
+                    }
+
+                }
+            }
+
+
+            FileStream sourceFile = new FileStream(Server.MapPath(Image1.ImageUrl.ToString()), FileMode.Open);
+            float FileSize;
+            FileSize = sourceFile.Length;
+            byte[] fileContent = new byte[(int)FileSize];
+            sourceFile.Read(fileContent, 0, (int)sourceFile.Length);
+            sourceFile.Close();
+            Response.ClearContent();
+            Response.ClearHeaders();
+            Response.Buffer = true;
+            Response.ContentType = "application/octet-stream";
+            Response.AddHeader("Content-Length", fileContent.Length.ToString());
+            Response.AddHeader("Content-Disposition", "attachment; filename=" + "encoded.jpg");
+            Response.BinaryWrite(fileContent);
+            Response.Flush();
+            Response.End();
+
+            //  SaveFileDialog saveFile = new SaveFileDialog();
+            /*  saveFile.Filter = "Image Files (*.png, *.jpg) | *.png; *.jpg";
+              saveFile.InitialDirectory = @"C:\Users\metech\Desktop";
+
+              if (saveFile.ShowDialog() == DialogResult.OK)
+              {
+                  textBoxFilePath.Text = saveFile.FileName.ToString();
+                  pictureBox1.ImageLocation = textBoxFilePath.Text;
+
+                  img.Save(textBoxFilePath.Text);
+              }*/
+        }
+
+
+        private void buttonDecode_Click(object sender, EventArgs e)
+        {
+            Bitmap img = new Bitmap("//Mac/Home/Pictures/Camera Roll/hi.png");
+            string message = "";
+
+            Color lastpixel = img.GetPixel(img.Width - 1, img.Height - 1);
+            int msgLength = lastpixel.B;
+
+            for (int i = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    Color pixel = img.GetPixel(i, j);
+
+                    if (i < 1 && j < msgLength)
+                    {
+                        int value = pixel.B;
+                        char c = Convert.ToChar(value);
+                        string letter = System.Text.Encoding.ASCII.GetString(new byte[] { Convert.ToByte(c) });
+
+                        message = message + letter;
+                    }
+                }
+            }
+
+             txtDecodedMsg.Text = message;
+        }
+
+    }
+}
